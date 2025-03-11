2ba8c2cd2b402886c80f444a9c639a95bb6d1ac3
remy.geles <remy.geles@etrs.terre.defense.gouv.fr>
2025-03-11 16:23:52 +0100

    Init features authent

---
+++ src/app/features/authent/authent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authent',
+  imports: [],
+  templateUrl: './authent.component.html',
+  styleUrl: './authent.component.css'
+})
+export class AuthentComponent {
+
+}
