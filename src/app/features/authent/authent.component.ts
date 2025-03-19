82ad1778d3ed6319257e6cc3595e04599903be30
tristan.keller <tristan.keller@etrs.terre.defense.gouv.fr>
2025-03-19 14:34:53 +0100

    Commit authentification fonctionnelle

--- src/app/features/authent/authent.component.ts
+++ src/app/features/authent/authent.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {AuthentService} from './authent.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -11,6 +13,27 @@ import {FormsModule} from '@angular/forms';
   styleUrls: ['./authent.component.css']
 })
 export class AuthentComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(
+    private authService: AuthentService,
+    private router: Router
+  ) {
+  }
+
+
+  login(): void {
+    const loginRequest = {username: this.username, password: this.password};
+    this.authService.login(loginRequest).subscribe({
+      next: (response: any) => {
+        this.router.navigate(['/accueil'])
+      },
+      error: (error: any) => {
+        console.error('Erreur de connexion:', error);
+      }
+    });
+  }
 }
+
 
